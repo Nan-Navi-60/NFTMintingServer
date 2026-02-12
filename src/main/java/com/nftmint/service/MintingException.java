91616987c92e541a71f9236dcd3f73ff0ed6f6d2
eujincho1 <yujinjo1@naver.com>
2026-02-12 15:05:22 +0900

    feat MintingException

--- src/main/java/com/nftmint/service/MintingException.java
+++ src/main/java/com/nftmint/service/MintingException.java
@@ -1,5 +1,25 @@
 package com.nftmint.service;
 
-public class MintingException {
+public class MintingException extends RuntimeException {
+    private final ErrorType errorType;
 
+    public enum ErrorType {
+        SYSTEM_ERROR,
+        INVALID_REQUEST,
+        CONCURRENCY_FAIL
+    }
+
+    public MintingException(String message) {
+        super(message);
+        this.errorType = ErrorType.SYSTEM_ERROR;
+    }
+
+    public MintingException(String message, ErrorType errorType) {
+        super(message);
+        this.errorType = errorType;
+    }
+
+    public ErrorType getErrorType() {
+        return errorType;
+    }
 }
